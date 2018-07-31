11b051b69c31f5eb2b9307da4fabba0f5efc928e
EC2 Default User <ec2-user@ip-172-31-9-92.us-east-2.compute.internal>
2018-07-31 20:50:22 +0000

    created about me quiz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,24 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/results' do
+    puts params
+    points_array = params.values 
+    total = 0
+    points_array.each do |point|
+      total += point.to_i
+    end
+    puts total
+    
+    result = calculate_result(total)
+    
+  if result == "Besties!! We should hang out!!"
+    return erb :bestfriends
+  elsif result == "I'll guess you'll have to do!"
+  return erb :okayfriends
+else 
+  return erb :notfriends
+  end
+end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,9 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+def calculate_result(point_total)
+    if point_total == 50
+        return "Besties!! We should hang out!!"
+    elsif point_total > 15 && point_total < 50 
+    return "I guess you'll have to do!"
+else
+    return "Gurlll we don't click at all! Bye Felica"
+    end
+end 
